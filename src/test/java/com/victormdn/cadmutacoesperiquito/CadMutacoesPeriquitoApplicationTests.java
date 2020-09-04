9c59f7ed7c9846ecc9537d49839ee0426cc727e1
Victor Matheus <victormatheusdonascimento@gmail.com>
2020-09-04 19:49:10 -0300

    add spring

---
+++ src/test/java/com/victormdn/cadmutacoesperiquito/CadMutacoesPeriquitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victormdn.cadmutacoesperiquito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadMutacoesPeriquitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
