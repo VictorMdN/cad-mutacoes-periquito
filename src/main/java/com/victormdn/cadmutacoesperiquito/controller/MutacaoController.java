7aa50c1265152bcfec803bc6d149f3b348d94454
Victor Matheus <victormatheusdonascimento@gmail.com>
2020-09-04 23:10:43 -0300

    add crud em multacoes

--- src/main/java/com/victormdn/cadmutacoesperiquito/controller/MutacaoController.java
+++ src/main/java/com/victormdn/cadmutacoesperiquito/controller/MutacaoController.java
@@ -3,7 +3,9 @@ package com.victormdn.cadmutacoesperiquito.controller;
 import com.victormdn.cadmutacoesperiquito.model.Mutacao;
 import com.victormdn.cadmutacoesperiquito.repository.MutacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,8 +20,32 @@ public class MutacaoController {
         return mutacaoRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Mutacao getById(@PathVariable Long id){
+        return mutacaoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
     @PostMapping
     public Mutacao post(@RequestBody Mutacao mutacao){
+        if (mutacao.getId() != null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O campo id deve estar em branco.");
         return mutacaoRepository.save(mutacao);
+    }
+
+    @PutMapping
+    public Mutacao put(@RequestBody Mutacao mutacao){
+        validateId(mutacao.getId());
+        return mutacaoRepository.save(mutacao);
+    }
+
+    @DeleteMapping("/{id}")
+    private void delete(@PathVariable Long id){
+        validateId(id);
+        mutacaoRepository.deleteById(id);
+    }
+
+    private void validateId(Long id){
+        if(id == null || !mutacaoRepository.findById(id).isPresent())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O campo id deve ser um valor já existente.");
     }
 }
